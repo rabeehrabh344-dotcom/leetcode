460e2cdc5e56b7592e77ae7417d54bcd96a06b1c
Muhammed Rabeeh KT <rabeehrabh344@gmail.com>
2026-09-07 12:10:56 +0530

    Time: 0 ms (100%), Space: 54.1 MB (14.95%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+function plusOne(digits) {
+
+  for(let i = digits.length-1; i >= 0; i--){
+
+    if (digits[i] < 9){
+        digits[i]++;
+        return digits;
+    }
+    digits[i] = 0;
+  }
+  digits.unshift(1)
+  return digits;
+}
+console.log(plusOne([1, 2, 3]));
